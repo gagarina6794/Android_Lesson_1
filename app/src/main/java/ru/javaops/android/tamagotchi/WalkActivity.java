1a2cbf6d94319cfcf56b42cade7a0a62a6c7f295
gagarina6794 <gagarina6794@gmail.com>
2019-02-15 16:43:12 +0200

    add move random pet in screen border

--- app/src/main/java/ru/javaops/android/tamagotchi/WalkActivity.java
+++ app/src/main/java/ru/javaops/android/tamagotchi/WalkActivity.java
@@ -3,17 +3,26 @@ package ru.javaops.android.tamagotchi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WalkActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private float height;
+    private int width;
+    private int startHeight;
+    private int startWidth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk);
-        ImageView imageView = findViewById(R.id.pet);
-        String pet = getIntent().getStringExtra("pet");
+        imageView = findViewById(R.id.pet);
+        String pet = getIntent().getStringExtra("pet").toLowerCase();
         int sourse;
 
         switch (pet) {
@@ -30,11 +39,49 @@ public class WalkActivity extends AppCompatActivity {
                 sourse = R.drawable.cthulhu;
         }
         imageView.setImageResource(sourse);
+
+        final FrameLayout frameLayout = findViewById(R.id.layout_walk);
+        ViewHelper.executeAfterViewHasDrawn(imageView, new Runnable() {
+            @Override
+            public void run() {
+                height = frameLayout.getHeight();
+                width = frameLayout.getWidth();
+                startAnimation();
+            }
+        });
     }
 
     public void onButtonClick(View view) {
         Intent intent = new Intent(WalkActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+
+   private Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            startAnimation();
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    };
+
+    private void startAnimation() {
+        int endHeight = (int) (Math.random() * (height + 1 - imageView.getHeight()));
+        int endWidth = (int) (Math.random() * (width + 1 - imageView.getWidth()));
+        Animation translateAnimation = new TranslateAnimation(startWidth, endWidth, startHeight, endHeight);
+        startHeight = endHeight;
+        startWidth = endWidth;
+        translateAnimation.setDuration(1000);
+        translateAnimation.setAnimationListener(animationListener);
+        imageView.startAnimation(translateAnimation);
     }
 
 }
